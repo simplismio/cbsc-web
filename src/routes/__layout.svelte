f720ca189fa43f0a40deb5cc60b10941c9b1a617
Joost <91443226+j0ost@users.noreply.github.com>
2021-10-08 23:41:44 +0630

    update

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,8 +7,8 @@
 <div class="h-screen flex flex-col items-center">
 	<nav class="text-center font-bold mt-10">
 		<a href="/" class="">Contracts</a>
-		<a href="/NewEventForm" class="ml-3">Create Events</a>
-		<a href="/NewCommitmentForm" class="ml-3">Create Commitments</a>
+		<a href="/NewEventForm" class="ml-3">+ Events</a>
+		<a href="/NewCommitmentForm" class="ml-3">+ Commitments</a>
 	</nav>
 
 	<slot />
