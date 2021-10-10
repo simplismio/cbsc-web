340f4125fc66022c38586a4abc506127fc6ea354
Joost <91443226+j0ost@users.noreply.github.com>
2021-10-10 15:29:39 +0630

    update

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,16 @@
 import { createClient } from '@supabase/supabase-js'
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const supabase = createClient(
     // @ts-ignore
-    import.meta.env.VITE_SUPABASE_URL,
+    process.env['VITE_SUPABASE_URL'],
+    process.env['VITE_SUPABASE_ANON_KEY']
+
+
+    //import.meta.env.,
     // @ts-ignore
-    import.meta.env.VITE_SUPABASE_ANON_KEY
+    //import.meta.env.
 )
 export default supabase;
